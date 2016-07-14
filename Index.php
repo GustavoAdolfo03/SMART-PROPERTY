f0bfc047e2015536a35ff547ae1ccd6fe488526b
Sebastian2119 <bscastillo26@misena.edu.co>
2016-07-14 13:13:30 -0500

    Ej1

---
+++ Index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+<style type="text/css">
+<!--
+.Estilo6 {
+	color: #000000;
+	font-style: italic;
+	font-family: "Courier New", Courier, monospace;
+	font-size: 40px;
+	font-weight: bold;
+}
+.Estilo7 {color: #000000}
+-->
+</style>
+</head>
+
+<body>
+<table width="827" height="393" border="1" align="center">
+  <tr>
+    <th height="121" colspan="2" bordercolor="#000000" bgcolor="#00CCFF" scope="col"><h1><span class="Estilo6">SMART PROPERTY</span></h1></th>
+  </tr>
+  <tr>
+    <td width="112" height="224"><em><img src="ImCasaim.jpg" width="240" height="161" /></em></td>
+    <td width="699"><p align="center" class="Estilo7">Nombre:
+        <input type="text" name="textfield" />
+      </p>
+      <p align="center" class="Estilo7"> Documento:
+        <input type="text" name="textfield2" />
+      </p>
+      <p align="center" class="Estilo7">
+        <input type="submit" name="Submit" value="Continuar" />
+      </p>
+      <form action="" method="post" name="form3" class="Estilo7" id="form3">
+        <label></label>
+      </form>
+      <form action="" method="post" name="form2" class="Estilo7" id="form2">
+        <label></label>
+      </form>
+      <form action="" method="post" name="form1" class="Estilo7" id="form1">
+        <label></label>
+      </form>    </td>
+  </tr>
+  <tr>
+    <td colspan="2">&nbsp;</td>
+  </tr>
+</table>
+<div align="center"></div>
+</body>
+</html>
